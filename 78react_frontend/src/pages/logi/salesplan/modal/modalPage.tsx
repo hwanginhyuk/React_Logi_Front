4c22f34449031a47c13a9cd328bb252dedd7a8cb
INHYUK\fit99 <fit9972@gmail.com>
2024-04-19 08:54:33 +0900

    판매계획 서비스 페이지 구현 시작

---
+++ 78react_frontend/src/pages/logi/salesplan/modal/modalPage.tsx
@@ -0,0 +1,10 @@
+import Modal from "pages/forms/plugins/frm-modal";
+
+
+export default function SalesModal({salesPlanNo}:any){
+    return(
+        <Modal>
+            salesPlanNo={salesPlanNo}
+        </Modal>
+    )
+}
